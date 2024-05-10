a9b02cd9c0b54c36fa9a628d8bc811e78ab067db
Fedotov <sasha4936@yandex.ru>
2024-05-10 14:55:22 +0500

    Initial Commit

---
+++ Nimak/NimakApp.swift
@@ -0,0 +1,17 @@
+//
+//  NimakApp.swift
+//  Nimak
+//
+//  Created by Fedotov Aleksandr on 10.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct NimakApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
